f1b687af687644d96843b1dfed21d792ea459589
Naz <post@nazs.net>
2020-04-29 07:37:14 +0100

    sticky footer works on IE11 too.

---
+++ pages/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="container">
+    <div class="content">
+      <h1>welcome</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores est deserunt numquam iusto asperiores voluptatum, pariatur quos dolorum ipsum vitae soluta delectus architecto ut, distinctio explicabo totam laborum earum porro?
+      </p>
+      <p style="display: none">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis itaque rerum a repellat in ad beatae explicabo necessitatibus repudiandae distinctio commodi sunt id ab excepturi quod accusamus, hic quis.
+        </p>
+    </div>
+    <footer>
+      <p>
+        &copy; The Footer
+      </p>
+    </footer>
+  </div>
+</template>
+
+<style >
+* {
+  margin: 0;
+  padding: 0;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content {
+  flex: 1 0 auto;
+}
+footer {
+  background-color: rgb(218, 214, 214);
+  height: 70px;
+  display: flex;
+  align-items: center;
+}
+
+</style>
